68ae145d466d318498fe9cf75d44846b7d889ad2
Pedro Barão <phbarao@gmail.com>
2021-05-04 17:22:59 -0300

    inputs and buttons

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TextInputProps } from 'react-native';
+
+import { Container, TextInput, Icon } from './styles';
+
+interface InputProps extends TextInputProps {
+  name: string;
+  icon: string; // receberemos o nome do ícone
+}
+
+const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
+  <Container>
+    <Icon name={icon} size={20} color="#666360" />
+
+    <TextInput
+      keyboardAppearance="dark"
+      placeholderTextColor="#666360"
+      {...rest}
+    />
+  </Container>
+);
+
+export default Input;
